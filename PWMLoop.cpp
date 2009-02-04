e7a91350c14e6ae107c911d377a1a3d502e4fe6a
mcclurem <mcclurem@9c7ebdb6-f3d1-4070-b6c4-c2ffb8923769>
2009-02-04 15:45:12 +0000

    Utility for adjusting the throttle with a pot on the 1st analog input of the DAS
    git-svn-id: file:///svnroot/fsaecontroller@7 9c7ebdb6-f3d1-4070-b6c4-c2ffb8923769

---
+++ PWMLoop.cpp
@@ -0,0 +1,26 @@
+
+#include "vcmdas.h"
+#include "time.h"
+#include "python_io.h"
+#include "string.h"
+
+int main(int argc, char *argv[]){
+
+	PythonIO* board= new PythonIO();
+	board->initPWM();
+	VCMDAS* das = new VCMDAS(0x300, 0x0000);
+	do
+	{
+		double value = das->getAnalog(0);
+		int outval = (int)(value * (18./5.)) + 22;
+		if(outval < 22)
+			outval = 22;
+		if(outval > 39)
+			outval = 39;
+		board->setPWMDuty(outval);
+		printf("Just set duty to %d\n", outval);
+	}while(true);
+
+	return 0;
+
+}
