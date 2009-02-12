2f51539e303c54e72b37367009985614a6a78d69
mcclurem <mcclurem@9c7ebdb6-f3d1-4070-b6c4-c2ffb8923769>
2009-02-12 21:51:28 +0000

    Modified the pwmloop program so that it won't run if the first digital in isn't shorted
    git-svn-id: file:///svnroot/fsaecontroller@10 9c7ebdb6-f3d1-4070-b6c4-c2ffb8923769

--- pwmloop.cpp
+++ pwmloop.cpp
@@ -7,8 +7,12 @@
 int main(int argc, char *argv[]){
 
 	PythonIO* board= new PythonIO();
-	board->initPWM();
 	VCMDAS* das = new VCMDAS();
+	unsigned char dig1 = board->getDigital(0);
+	if(!(dig1 & 0x01))
+		return 0;
+	
+	board->initPWM();
 	do
 	{
 		double value = das->getAnalog(0);
@@ -18,7 +22,10 @@ int main(int argc, char *argv[]){
 		if(outval > 39)
 			outval = 39;
 		board->setPWMDuty(outval);
-		printf("Just set duty to %d\n", outval);
+		dig1 = board->getDigital(0);
+		if(!(dig1 & 0x01))
+				return 0;
+		//printf("Just set duty to %d\n", outval);
 	}while(true);
 
 	return 0;
