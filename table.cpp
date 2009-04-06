0d189233eb9774b848a65219822921b2274d03d9
mcclurem <mcclurem@9c7ebdb6-f3d1-4070-b6c4-c2ffb8923769>
2009-04-06 17:46:39 +0000

    Working on making linear (planar) interpolation work
    git-svn-id: file:///svnroot/fsaecontroller@28 9c7ebdb6-f3d1-4070-b6c4-c2ffb8923769

--- table.cpp
+++ table.cpp
@@ -1,61 +1,153 @@
 
 #include "table.h"
 
-Table::Table(int size_a, int size_b, double delt_a, double delt_b)
+struct point
 {
-		//values = new vector< vector<double> >;
-		//values = (double*)malloc(sizeof(double[100])*100);
-		values[0][0] = 0.;
+		double x,y,z;
+};
+
+
+Table::Table(int size_x, int size_y, double delt_x, double delt_y)
+{
+		delta_x = delt_x;
+		delta_y = delt_y;
+		length_x = size_x;
+		length_y = size_y;
+		
+//Initialize the values array
+		values = (double **)calloc(sizeof(double *), length_x);
+		values[0] = (double *)calloc(sizeof(double), length_x * length_y);
+		for(int i = 1; i < length_x; i++)
+			values[i] = values[0] + i * length_y;
+
+
 }
 
-double Table::lookup(double value_a, double value_b)
+void Table::edit(int x, int y, double value)
+{
+		values[x][y] = value;
+}
+
+double Table::lookup(double value_x, double value_y)
 {
 		//*********NOTE**********
 		//We have to be certain that these catch statements are all
 		//inclusive - a seg-fault on this code will be FATAL!!!
 
-		//Casting to an int will truncate and get our next closest value
-		//below our actual (then subtract 1 because array starts at zero)
-		int index_a1 = (int)(value_a / delta_a) - 1;
-		//Catch small values
-		if(index_a1 < 0)
-			index_a1 = 0;
+		if(value_x < 0.)
+				value_x = 0.;
+		if(value_x > (length_x - 1) * delta_x)
+				value_x = (length_x - 1) * delta_x;
+		if(value_y < 0.)
+				value_y = 0.;
+		if(value_y > (length_y - 1) * delta_y)
+				value_y = (length_y - 1) * delta_y;
+
 		
-		int index_a2 = index_a1 + 1; //The next highest value
+		//Casting to an int will truncate and get our next closest value
+		//below our actual (then subtract 1 because array starts at zero)
+		int index_x1 = (int)(value_x / delta_x);
+		//Catch small values
+		//if(index_x1 < 0)
+		//	index_x1 = 0;
+		
+		int index_x2 = index_x1 + 1; //The next highest value
 		//Catch large values
-		if(index_a2 > length_a - 1)
-			index_a2 = length_a - 1;
+		if(index_x2 > length_x - 1)
+			index_x2 = length_x - 1;
+
 
 		//Casting to an int will truncate and get our next closest value
 		//below our actual (then subtract 1 because array starts at zero)
-		int index_b1 = (int)(value_b / delta_b) - 1;
+		int index_y1 = (int)(value_y / delta_y);
 		//Catch small values
-		if(index_b1 < 0)
-			index_b1 = 0;
-		int index_b2 = index_b1 + 1; //The next highest value
+		//if(index_y1 < 0)
+		//	index_y1 = 0;
+		int index_y2 = index_y1 + 1; //The next highest value
 		//Catch large values
-		if(index_b2 > length_b - 1)
-			index_b2 = length_b - 1;
+		if(index_y2 > length_y - 1)
+			index_y2 = length_y - 1;
+
+
+		printf("\n\n\nIndex a1 = %d, Index b1 = %d\n", index_x1, index_y1);
+
+		double value_x0y0 = values[index_x1][index_y1];
+		double value_x0y1 = values[index_x1][index_y2];
+		double value_x1y0 = values[index_x2][index_y1];
+		double value_x1y1 = values[index_x2][index_y2];
+
+		double midval = (value_x0y0 + value_x0y1 + value_x1y0 + value_x1y1)/4.;
+
+		//We need to solve the plane eq
+		//Ax+By+Cz=D
+		//We are working in a left handed coord system
+		
+		double x = 1.- (((double)(index_x2)*delta_x - value_x)/delta_x);
+		double y = 1.- (((double)(index_y2)*delta_y - value_y)/delta_y);
+
+		printf("X=%lf, Y=%lf\n",x,y);
+		point interpoint, point1, point2, point3;
+		if( y <= x )
+		{//upper right
+			printf("UR\n");
+			point1.x = 1.;
+			point1.y = 0.;
+			point1.z = value_x1y0;
+		}else
+		{//lower left
+			printf("LL\n");
+			point1.x = 0.;
+			point1.y = 1.;
+			point1.z = value_x0y1;
+		}
+		
+		if( y <= (-x + 1) )
+		{//upper left
+			printf("UL\n");
+			point2.x = 0.;
+			point2.y = 0.;
+			point2.z = value_x0y0;
+		}else
+		{//lower right
+			printf("LR\n");
+			point2.x = 1.;
+			point2.y = 1.;
+			point2.z = value_x1y1;
+		}
+
+		point3.x = 0.5;
+		point3.y = 0.5;
+		point3.z = midval;
+		
+		interpoint.x = x;
+		interpoint.y = y;
+
+
+		printf("point 1=%lf, point2=%lf 2midval = %lf\n", point1.z, point2.z, midval);
+		//Calculate determinants
+		double A,B,C,D;
+		A = (point1.y * (point2.z - point3.z)) + (point2.y * (point3.z - point1.z)) + (point3.y * (point1.z - point2.z));
+		B = (point1.z * (point2.x - point3.x)) + (point2.z * (point3.x - point1.x)) + (point3.z * (point1.x - point2.x));
+		C = (point1.x * (point2.y - point3.y)) + (point2.x * (point3.y - point1.y)) + (point3.x * (point1.y - point2.y));
+		D = (point1.x * (point2.y * point3.z - point3.y * point2.z))
+			+ (point2.x * (point3.y * point1.z - point1.y * point3.z))
+			+ (point3.x * (point1.y * point2.z - point2.y * point1.z));
+
+		printf("A=%lf, B=%lf, C=%lf, D=%lf\n",A,B,C,D);
+		interpoint.z = ( D - ( interpoint.x * A )-( interpoint.y * B))/C;
 
 
 
-		//We are now going to determine where the index would be fractionally
-		double percent_a1 = (value_a - (double)(index_a1*delta_a))/delta_a;
-		double percent_a2 = 1. - percent_a1;
 
-		double percent_b1 = (value_b - (double)(index_b1*delta_b))/delta_b;
-		double percent_b2 = 1. - percent_b1;
 
-		double value_a1b1 = values[index_a1][index_b1];
-		double value_a1b2 = values[index_a1][index_b2];
-		double value_a2b1 = values[index_a2][index_b1];
-		double value_a2b2 = values[index_a2][index_b2];
 
-		//Now we do a weighted average of all the edges and divide by 4 and sum
-		double val_top = value_a1b1 * percent_a1 + value_a2b1 * percent_a2;//Top segment
-		double val_bottom = value_a1b2 * percent_a1 + value_a2b2 * percent_a2;//Bottom segment
-		double val_left = value_a1b1 * percent_b1 + value_a1b2 * percent_b2;//Left Segment
-		double val_right =value_a2b1 * percent_b1 + value_a2b2 * percent_b2;//Right Segment
 
-		return ((val_top + val_bottom + val_left + val_right)/4.);
+
+
+
+
+
+		printf("%lf\t%lf\n%lf\t%lf\n", value_x0y0, value_x1y0, value_x0y1, value_x1y1);
+		
+		return interpoint.z;
 }
--- table.h
+++ table.h
@@ -5,13 +5,14 @@ using namespace std;
 class Table
 {
 	public:
-		Table(int size_a, int size_b, double delt_a, double delt_b);
-		double lookup(double value_a, double value_b);
+		Table(int size_x, int size_y, double delt_x, double delt_y);
+		void edit(int x, int y, double value);
+		double lookup(double value_x, double value_y);
 	private:
-		//vector< vector<double> > * values;
-		double values[100][100];
-		int length_a;//The length of the a-dimension of the array
-		int length_b;//The length of the b-dimension of the array
-		double delta_a;//The spacing of the a indices
-		double delta_b;//The spacing of the b indices
+//		vector< vector<double> > values;
+		double **values;
+		int length_x;//The length of the a-dimension of the array
+		int length_y;//The length of the b-dimension of the array
+		double delta_x;//The spacing of the a indices
+		double delta_y;//The spacing of the b indices
 };
