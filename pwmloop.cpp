54395a748f7a12ef525ca3e5ad7808f288b6dd22
mcclurem <mcclurem@9c7ebdb6-f3d1-4070-b6c4-c2ffb8923769>
2009-08-24 01:26:54 +0000

    More changes
    git-svn-id: file:///svnroot/fsaecontroller@49 9c7ebdb6-f3d1-4070-b6c4-c2ffb8923769

--- pwmloop.cpp
+++ pwmloop.cpp
@@ -8,39 +8,64 @@ int main(int argc, char *argv[]){
 
 	PythonIO* board= new PythonIO();
 	VCMDAS* das = new VCMDAS();
-	board->initDigitalIO(0xFFFFFF00);
-	
-	/*for(unsigned char i=0; i<=255; i++)
+/*	board->initDigitalIO(0xFFFFFF00);
+
+	for(unsigned char i=0; i<=255; i++)
 	{
 			if(board->getDigital(1) < 255)
-				usleep(100000);
+			{
+				unsigned char bar = (board->getDigital(1));
+				int foo = 1;
+				for(int j=0; j<=7;j++)
+				{
+						if(bitof(j, bar))
+							foo *=2;
+				}
+				usleep(1000 * foo);
+			}
 			else
 				usleep(1000000);
 			board->setDigital(0,i);
-	}*/
+	}
 	do
 	{
 			board->setDigital(0, board->getDigital(1));
-	}while(true);
+	}while(true);*/
+
 	unsigned char dig1 = board->getDigital(0);
-	unsigned char dig2 = board->getDigital(1);
-	printf("%d\t%d\n", dig1, dig2);
-//	if(!(dig1 & 0x01))
+	board->initDigitalIO(0xFFFF00FF);
+	if(dig1 != 255)
 		return 0;
 	
+	int oldval=0;
 	board->initPWM();
-	do
+	for(unsigned char i=0; i <=255; i++)
 	{
-		double value = das->getAnalog(0);
-		int outval = (int)(value * (18./5.)) + 22;
-		if(outval < 22)
-			outval = 22;
-		if(outval > 39)
-			outval = 39;
-		board->setPWMDuty(outval);
+			double value = das->getAnalog(0);
+			int outval = (int)(value * (19./5.)) + 22;
+			if(outval < 22)
+				outval = 22;
+			if(outval > 40)
+				outval = 40;
+			if(outval != oldval)
+			{
+				board->setPWMDuty(outval);
+				//if( abs(oldval-outval) > 2)
+				//	printf("Old=%d, New=%d, Value=%f\n", oldval, outval, value);
+				oldval = outval;
+			}
+//		usleep(10);
+		board->setDigital(1, (outval - 21));
 		dig1 = board->getDigital(0);
-		if(!(dig1 & 0x01))
+		if(dig1 != 255)
+		{
+			dig1 = board->getDigital(0);
+			if(dig1 != 255)
+			{
+				board->setDigital(1, dig1);
 				return 0;
+			}
+		}
 		//printf("Just set duty to %d\n", outval);
 	}while(true);
 
